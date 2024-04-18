28d77c1ad229c6a51ff7d3d4fe4d9a5b8fc506cd
Gouravrohilla33 <147707728+Gouravrohilla33@users.noreply.github.com>
2024-04-18 16:08:41 +0530

    .

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,8 +1,4 @@
-<?php
 
-// include "../config.php";
-
-?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light ">
 <div class="container-fluid">
   <a class="navbar-brand" href="#">Quiz Manager</a>
